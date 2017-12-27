901406a826ee8e6b84d1d26fc3e93f4502ec10d7
Moises Barrera <moises.barrera@pricetravel.com.mx>
2017-12-26 19:04:34 -0500

    sección 2, clase 10 parte 2

--- TypeScript ES6 practicas/introducción/app.js
+++ TypeScript ES6 practicas/introducción/app.js
@@ -1,10 +1,17 @@
 "use strict";
+var OPCIONES = "todas";
 // nombre tiene que ser una variable
 // de tipo texto
 function saludar(nombre) {
     console.log("Hola " + nombre.toUpperCase());
+    console.log(OPCIONES);
 }
 var wolverine = {
     nombre: "Logan"
 };
 saludar(wolverine.nombre);
+var mensaje = "hola";
+if (true) {
+    var mensaje_1 = "adios";
+}
+console.log(mensaje);
--- TypeScript ES6 practicas/introducción/app.ts
+++ TypeScript ES6 practicas/introducción/app.ts
@@ -1,12 +1,25 @@
+
+const OPCIONES = "todas";
+
 // nombre tiene que ser una variable
 // de tipo texto
 function  saludar (nombre:string)
 {
   console.log("Hola " + nombre.toUpperCase());
+  console.log(OPCIONES);
 }
 
-var wolverine = {
+let wolverine = {
   nombre: "Logan"
 };
 
 saludar(wolverine.nombre);
+
+let mensaje = "hola";
+
+if(true)
+{
+  let mensaje = "adios";
+}
+
+console.log(mensaje);
