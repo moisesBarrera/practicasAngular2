e058791d06e5ab227a37e7d3ef0b83bb4da4c5dc
Moises Barrera <moises.barrera@pricetravel.com.mx>
2017-12-27 19:50:28 -0500

    seccion 2, clase 16

--- TypeScript ES6 practicas/introducción/app.js
+++ TypeScript ES6 practicas/introducción/app.js
@@ -15,3 +15,81 @@ if (true) {
     var mensaje_1 = "adios";
 }
 console.log(mensaje);
+//Tipos de variables, Sección 2, Clase 12
+var nombre = "Peter";
+var numero = 12;
+var booleano = true;
+var hoy = new Date();
+hoy = new Date("2010-10-21");
+console.log(hoy);
+var cualquiera = nombre;
+cualquiera = numero;
+cualquiera = booleano;
+cualquiera = hoy;
+var spiderman = {
+    nombre: "Peter",
+    edad: 20
+};
+//Templates Literarios sección 2, clase 13
+function getNombre() {
+    return "Fernando";
+}
+;
+nombre = "Juan";
+var apellido = "Perez";
+var edad = 32;
+//let texto = "hola " + nombre + " " + apellido;
+var texto = "Hola,\n" + nombre + " " + apellido + "\n(" + edad + ")";
+var texto2 = 1 + 2 + ", " + getNombre();
+console.log(texto);
+console.log(texto2);
+//Parametros obligatorios, por defecto y opcionales.
+function activar(quien, //obligatorio
+    objecto, // defecto
+    momento) {
+    if (objecto === void 0) { objecto = "Batiseñal"; }
+    var mensaje;
+    if (momento) {
+        mensaje = quien + " activ\u00F3 la " + objecto + " en la " + momento;
+    }
+    else {
+        mensaje = quien + " activ\u00F3 la " + objecto;
+    }
+    console.log(mensaje);
+}
+activar("Gordon");
+activar("Gordon", "BATISEÑAL");
+activar("Gordon", "batiseñal", "tarde");
+// Funciones de flecha
+var miFuncion = function (a) {
+    return a;
+};
+var miFuncionF = function (a) { return a; };
+var miFuncion2 = function (a, b) {
+    return a + b;
+};
+var miFuncion2F = function (a, b) { return a + b; };
+var miFuncion3 = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var miFuncion3F = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var hulk = {
+    nombre: "Hulk",
+    smash: function () {
+        var _this = this;
+        setTimeout(function () {
+            console.log(this.nombre + " smash!!"); //En este caso el this apunta al global que es el window
+        }, 1500);
+        setTimeout(function () {
+            return console.log(_this.nombre + " smash!!");
+        } // En la función de flecha el this apunta al this local del objeto hulk
+        , 1500);
+    }
+};
+hulk.smash();
+console.log(miFuncion("Normal"));
+console.log(miFuncionF("Flecha"));
--- TypeScript ES6 practicas/introducción/app.ts
+++ TypeScript ES6 practicas/introducción/app.ts
@@ -23,3 +23,110 @@ if(true)
 }
 
 console.log(mensaje);
+
+//Tipos de variables, Sección 2, Clase 12
+let nombre:string = "Peter";
+let numero:number = 12;
+let booleano:boolean = true;
+let hoy:Date = new Date();
+
+hoy = new Date("2010-10-21");
+
+console.log(hoy);
+
+let cualquiera:any = nombre;
+cualquiera =  numero;
+cualquiera = booleano;
+cualquiera = hoy;
+
+let spiderman = {
+  nombre: "Peter",
+  edad: 20
+};
+
+//Templates Literarios sección 2, clase 13
+
+function getNombre()
+{
+  return "Fernando";
+};
+
+nombre = "Juan";
+let apellido:string = "Perez";
+let edad:number = 32;
+
+//let texto = "hola " + nombre + " " + apellido;
+
+let texto = `Hola,
+${ nombre } ${ apellido }
+(${ edad })`;
+
+
+let texto2:string = `${1 + 2}, ${ getNombre() }`;
+
+console.log(texto);
+
+console.log(texto2);
+
+//Parametros obligatorios, por defecto y opcionales.
+
+function activar( quien: string,  //obligatorio
+                    objecto:string = "Batiseñal", // defecto
+                      momento?:string){ //optional
+  let mensaje:string;
+
+  if( momento )
+  {
+    mensaje = `${ quien } activó la ${ objecto } en la ${ momento }`;
+  } else {
+    mensaje = `${ quien } activó la ${ objecto }`;
+  }
+
+  console.log(mensaje);
+}
+
+activar("Gordon" );
+activar("Gordon", "BATISEÑAL");
+activar("Gordon", "batiseñal", "tarde");
+
+// Funciones de flecha
+
+let miFuncion = function( a ){
+  return a;
+};
+
+let miFuncionF = ( a ) => a;
+
+let miFuncion2 = function ( a:number, b:number){
+  return a + b;
+}
+
+let miFuncion2F = (a:number, b:number) => a + b;
+
+let miFuncion3 = function( nombre:string ){
+  nombre = nombre.toUpperCase();
+  return nombre;
+}
+
+let miFuncion3F = ( nombre:string ) => {
+  nombre = nombre.toUpperCase();
+  return nombre;
+}
+
+let hulk = {
+  nombre: "Hulk",
+  smash(){
+    setTimeout(function(){
+      console.log(this.nombre + " smash!!"); //En este caso el this apunta al global que es el window
+    },1500);
+
+      setTimeout(() =>
+        console.log(this.nombre + " smash!!") // En la función de flecha el this apunta al this local del objeto hulk
+      ,1500);
+  }
+}
+
+hulk.smash();
+
+console.log( miFuncion("Normal") );
+console.log( miFuncionF("Flecha") );
